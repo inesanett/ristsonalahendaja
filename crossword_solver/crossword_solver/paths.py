93d30cbcb0afedf2f1f835a8263230c012b34bd7
Ines <inesanett@gmail.com>
2024-05-08 16:48:40 +0300

    font update

--- crossword_solver/crossword_solver/paths.py
+++ crossword_solver/crossword_solver/paths.py
@@ -5,3 +5,4 @@ DATA_FOLDER_PATH = 'C:/Users/ianigol/Documents/MAGISTER/ristsonalahendaja/crossw
 
 WORDNET_VOCAB_PATH = os.path.join(DATA_FOLDER_PATH, 'wordnet_contents.parquet')
 WORD2VEC_PATH = os.path.join(DATA_FOLDER_PATH, 'lemmas.cbow.s100.w2v.bin')
+FONT_PATH = os.path.join(DATA_FOLDER_PATH, 'Roboto-Regular.ttf')
